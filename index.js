e8c1343dc0953fbb06cea3e228fdca641aa5d7d1
George  Pianka <gpianka1@gmail.com>
2019-04-08 10:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,17 @@ function distanceTravelledInFeet (startBlock, endBlock){
     }
 }
 
+function calculatesFarePrice (start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance >= 400 && distance <= 2000) {
+    return .02 * ( distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
 
