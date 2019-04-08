8e5d1783d9e43f50f61d56da7696cc0b5fa8a0d6
George  Pianka <gpianka1@gmail.com>
2019-04-08 10:02:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (blockNumber) {
+  if  (blockNumber > 42) {  
+    return blockNumber - 42 
+  } 
+  if (blockNumber < 42){  
+    return 42 - blockNumber }
+}
+
